2619d2be337fa67d0218656049de15c7b8084052
Nic Bhasin <newagequanta@gmail.com>
2017-09-02 03:30:50 +0000

    keytest

---
+++ keytest.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+TRIES=0
+
+while [[ $TRIES -lt 3 ]]; do
+	echo "Enter some keys"
+	read KEYS
+
+	echo $KEYS
+
+	echo "Do you see the keys as you typed them? (y or n)"
+	read RESPONSE
+	if [[ $RESPONSE = "y" ]]; then
+		echo "Test successful!"
+		exit 0
+	else
+		echo "Try again"
+		TRIES="$(( TRIES+1 ))"
+	fi
+done
+
+echo "Please take you keyboard in for servicing"
